2c5d27ae92ae58bd14bef3b720a78399da75de66
Rumi Islam <rumi16625@gmail.com>
2025-08-03 12:05:09 -0700

    player and enemy class additions

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,7 +1,39 @@
-export class Enemy
+export class Monster
 {
-    constructor(spritesheet, posX, posY, hp,)
-    {
-
-    }
+	constructor(spritesheet, position, damage, hitbox)
+	{
+		this.spritesheet = spritesheet;//image
+		this.position = position;//pair of coordinates
+		this.hitbox = hitbox;//rectangle around monster, 4 coordinates
+		this.hit = damage;
+		this.strongHit = damage*1.5;
+	}
+/*
+	checkPlayerInRange(player)
+	{
+		
+		if(player hitbox overlaps monster hitbox)
+		this.attacks(player);
+		
+	}
+	meleeAttacks()
+	{
+		if(Math.random() < 0.10)
+		{
+			return this.strongHit;
+		}
+		else{
+			return this.hit;
+		}
+		//wait 3 seconds before attacking, stop moving for 1 second
+	}
+*/
+}
+export class BasicMonster extends Monster
+{
+	constructor(spritesheet, health)
+	{
+		super(spritesheet, position, damage);
+		this.health = health;
+	}
 }
--- js/Player.js
+++ js/Player.js
@@ -1,7 +1,21 @@
 export class Player
 {
-	constructor(playerID, playerName, spritesheet, posX, posY, )
+	constructor(playerID, playerName, spritesheet, position, speed)
 	{
+		this.playerID = playerID;
+		this.playerName = playerName;
+		this.spritesheet = spritesheet;
+
+		this.position = position;
+		this.walkSpeed = speed;
+		this.sprintSpeed = speed*2;
+		this.crawlSpeed = speed/2;
+
+		this.health = 100;
 		
+
+		this.inventory = null;
+		this.armor = null;
+		this.weapon = null;
 	}
 }
